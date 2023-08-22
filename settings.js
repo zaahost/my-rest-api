a821b87f746690ffd252efd01ea9532b24235329
zaahost <116005760+zaahost@users.noreply.github.com>
2023-08-22 17:16:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'RamzLvr'// yourname
+global.MONGO_DB_URI = "mongodb+srv://RamzLover:rbecd7fTFWray9jJ@cluster0.tzsau2j.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "stokakun8137@gmail.com" //email
+global.email_password = "nhzktcawtphsoocj" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
